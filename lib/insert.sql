02de58f692c6c943859e8ed85565a9dbebaaa64c
Stela Capsa <stela.capsa@gmail.com>
2020-02-19 20:49:31 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Harry Potter", "Movie", 500, "01/16/22", "01/22/22"); 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Toys", "Donation", 1000, "/05/17/20", "05/30/20"); 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Plantating trees", "Echology", 2500, "06/02/20", "10/20/20") 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Movie in the park", "Movie", 8, "05/20/20", "08/20/20") 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Plantating trees", "Echology", 2500, "06/02/20", "10/20/20") ;
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Movie in the park", "Movie", 8, "05/20/20", "08/20/20"); 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Saving the light", "Echologic", 1, "04/05/21", "04/06/21"); 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Picking the coffee", "Coffee feelds", 200, "05/13/20", "11/22/20");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Food for holess", "Donation", 300, "07/01/22", "08/30/22"); 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Hospital blankets", "Donation", 1500, "04/22/21", "09/30/21");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Ocean cleaning", "Echologiy", 5000, "10/22/20", "11/30/21" );
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("")
 
 
 INSERT INTO users (name, age) VALUES
