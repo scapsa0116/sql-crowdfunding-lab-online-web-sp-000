44f54738b161d779d1ab92f355d914d2b3022985
Stela Capsa <stela.capsa@gmail.com>
2020-02-19 20:34:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES("Harry Potter", "Movie", 500, "01/16/22", "01/22/22"); 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES("Toys", "Donation", 1000, "/05/17/2020", "05/30/2020"); 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Harry Potter", "Movie", 500, "01/16/22", "01/22/22"); 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Toys", "Donation", 1000, "/05/17/20", "05/30/20"); 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Plantating trees", "Echology", 2500, "06/02/20", "10/20/20") 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Movie in the park", "Movie", 8, "05/20/20", "08/20/20") 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
