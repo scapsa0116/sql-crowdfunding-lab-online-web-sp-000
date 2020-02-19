5e702cf1236ce51b7aee9c94cf1f17faa6293ba8
Stela Capsa <stela.capsa@gmail.com>
2020-02-19 21:17:34 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,8 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT user.name, user.age, SUM (pledge.amount) FROM users
   INNER JOIN pledges
-  ON user.id = pledge."
+  ON user.id = pledge.user_id 
+  
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
