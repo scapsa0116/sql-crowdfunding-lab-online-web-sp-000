44ba92fbf3e6f18e138a022991477f8133a0a35e
Stela Capsa <stela.capsa@gmail.com>
2020-02-19 20:16:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES() 
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES("harry Potter", "Movie", "Children", 01/16/22, 01/22/22) 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
